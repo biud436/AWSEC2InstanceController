4112c06ccbe63b22eeae923f793463a6d75227b5
Ubuntu <ubuntu@ip-172-31-41-86.ap-northeast-2.compute.internal>
2021-11-09 18:02:07 +0900

    fixed

--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,4 @@
 INDEX_FILE_PATH=$(readlink -f ../src/index.rb)
 INDEX_FILE_DIR=$(dirname $INDEX_FILE_PATH)
-echo "alias aws_ec2_simple_connector=ruby $INDEX_FILE_PATH" >> ~/.bashrc
+echo "alias aws_ec2_simple_connector=\"ruby $INDEX_FILE_PATH\"" >> ~/.bashrc
 source ~/.bashrc

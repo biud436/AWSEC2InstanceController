1eecd3eff0777c6cb6763a3adf894d0962fcdb99
biud436 <biud436@gmail.com>
2021-11-09 18:18:31 +0900

    fixed "source ~/.bashrc"

--- bin/setup-aws-cli.sh
+++ bin/setup-aws-cli.sh
@@ -5,4 +5,4 @@ docker run --rm -it amazon/aws-cli --version
 
 echo "Setting up the file named .bashrc"
 echo alias aws='docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli' >> ~/.bashrc
-source .bashrc
+source ~/.bashrc

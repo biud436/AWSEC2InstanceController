20a6eb96ce0eac82d188ab0a68fd6d6c5fdebeaa
biud436 <biud436@gmail.com>
2021-11-09 15:42:39 +0900

    fixed the sh file

--- bin/setup-aws-cli.sh
+++ bin/setup-aws-cli.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Installing aws-cli using docker....."
 docker run --rm -it amazon/aws-cli --version
+
+echo "Setting up the file named .bashrc"
 echo alias aws='docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli' >> ~/.bashrc
 source .bashrc
